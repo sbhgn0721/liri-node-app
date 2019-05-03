306d263b4f74eed5f8d6bebbcfed6bdcf7227cf5
hongliao xiong <hongliaoxiong@D-10-19-210-106.dhcp4.washington.edu>
2019-05-02 20:44:00 -0700

    make spotify-this-song and do-what-it-says work

--- liri.js
+++ liri.js
@@ -51,62 +51,57 @@ function liriBot(userCommand, userInput) {
 };
 
 //spotify-this-song part
-function spotifyThisSong(songName) {
+function spotifyThisSong(userInput) {
     var spotify = new Spotify(keys.spotify);
-    //console.log("Spotify key: " + spotify);
+    //console.log(keys.spotify);
 
-    if (!songName) {
-        songName = "The Sign";
+
+    if (!userInput) {
+        userInput = "The Sign";
     } else {
-        spotify.search({ type: "track", query: songName }, function (err, data) {
+        spotify.search({ type: "track", query: userInput }, function (err, data) {
             if (err) {
                 console.log('Error occurred: ' + err);
             } else {
+                
                 console.log("The following information is about this song:");
-                console.log("Artist Name: " + data.tracks.items[0].album.artist[0].name + "\nThe song's name: " + data.tracks.items[0].name + "\nA preview link of the song from Spotify: " + data.tracks.items[0].href
-                    + "\nThe album that the song is from: " + data.tracks.items[0].album.name)
+                console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\nThe song's name: " + data.tracks.items[0].album.name + "\nA preview link of the song from Spotify: " + data.tracks.items[0].album.href
+                 + "\nThe album that the song is from: " + data.tracks.items[0].album.name)
             }
         });
-    }
+   }
 }
 
 //concert-this artist/band name part
-function concertThis(artist) {
-    var artist = userInput;
-    var queryURLForBandsInTownAPI = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+function concertThis(userInput) {
+   
+    var queryURLForBandsInTownAPI = "https://rest.bandsintown.com/artists/" + userInput+ "/events?app_id=codingbootcamp"
 
-    if (artist === "") {
-        //console.log("You haven't put the name of the artist or band.")
-    } else {
-        axios.get(queryURLForBandsInTownAPI).then(
-            function (bands) {
-                console.log("The following information is about the band/artist:");
-                console.log(bands);
-                console.log("Name of the artist/band: " + artist + "\nName of the venue: " + bands.data[0].venue.name + "\nVenue location: " + bands.data[0].venue.city + "\nDate of the Even: " + moment(bands.data[0].datetime).format("MM-DD-YYYY"));
-            }
-        )
+    axios.get(queryURLForBandsInTownAPI).then(
+        function (response) {
+            console.log("The following information is about the band/artist:");
+            //console.log(response);
+            console.log("Name of the artist/band: " + userInput + "\nName of the venue: " + response.data[0].venue.name + "\nVenue location: " + response.data[0].venue.city + "\nDate of the Even: " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
+        }
+    )
 
-
-    }
 }
 
 
-
-
 //movie-this movieName part
-function movieThis(movieName) {
-    var movieName = userInput;
-    var queryUrlForOMDBAPI = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    if (movieName === "") {
-        movieName = "Mr. Nobody"
+function movieThis(userInput) {
+    
+    if (!userInput) {
+        userInput = "Mr. Nobody";
     } else {
-        axios.get(queryUrlForOMDBAPI).then(
+        var queryUrlForOMDBAPI = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+        axios.request(queryUrlForOMDBAPI).then(
             function (response) {
+                //console.log(response)
                 console.log("The following information is about the movie:");
-                console.log("Title of the movie: " + movieName + "\nYear the movie came out: " + response.data.Year + "\nIMDB Rating of the movie: " + response.data.imdbRating
-                    + "\nRotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value + "\nCountry where the movie was produced: " + response.data.Country + "\nLanguage of the movie: " + response.data.Language + "\nPlot of the movie: " + response.data.Plot
-                    + "\nActors in the movie: " + response.data.Actors);
+                console.log("Title of the movie: " + userInput+ "\nYear the movie came out: " + response.data.Year + "\nIMDB Rating of the movie: " + response.data.imdbRating
+                  + "\nRotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value + "\nCountry where the movie was produced: " + response.data.Country + "\nLanguage of the movie: " + response.data.Language + "\nPlot of the movie: " + response.data.Plot
+                  + "\nActors in the movie: " + response.data.Actors);
             })
     }
 }
@@ -123,8 +118,8 @@ function getRandom() {
             var randomData = data.split(",");
             liriBot(randomData[0], randomData[1]);
         }
-    })
-}
+    });
+};
 
 liriBot(userCommand, userInput);
 
